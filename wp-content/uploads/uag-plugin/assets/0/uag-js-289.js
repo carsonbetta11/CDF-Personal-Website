10ef1e2e345b4cf515cc70517b827a0fb84e7787
carsonbetta11 <chris.frago@Outlook.com>
2026-05-27 14:19:34 -0500

    Initial Website Upload 5/27/26
    Initial upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-289.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e7443608' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-18ea574f' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-0c12f1e9' );
+});
+ });
